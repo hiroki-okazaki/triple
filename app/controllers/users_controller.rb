1c380db676b9c9f2436ca61e56ef29c6e72d3f24
hiroki-okazaki <1031okazaki@gmail.com>
2019-03-23 18:05:33 +0900

    Active Storageの実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -47,6 +47,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:name, :profile)
+      params.require(:user).permit(:name, :age, :region, :profile, :new_image)
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ApplicationRecord
+  has_one_attached :image
+  attribute :region
+  attribute :new_image
+
   validates :name, presence: true, length: { maximum: 30 }
   validates :profile, presence: true, length: { maximum: 1000 }
+  validates :new_image, presence: true
+
+  before_save do
+    self.image = new_image if new_image
+  end
 end
